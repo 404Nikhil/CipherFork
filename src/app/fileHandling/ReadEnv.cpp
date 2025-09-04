315c0c739a585dd45745ec22e3265eb257e6584d
404Nikhil <nicksd111@gmail.com>
2025-09-05 00:45:52 +0530

    feat: IO:file_handling init

---
+++ src/app/fileHandling/ReadEnv.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <IO.hpp>
+#include <fstream>
+#include <sstream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class ReadEnv {
+    public:
+        string getenv(){
+            string env_path = ".env";
+            IO io(env_path);
+            fstream file_stream = io.getFileStream();
+            stringstream buffer;
+            buffer << file_stream.rdbuf();
+            string file_content = buffer.str();
+            return file_content;
+        }
+}
